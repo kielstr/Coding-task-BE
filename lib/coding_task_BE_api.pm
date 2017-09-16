5e5dbc020a78bd390dd71b19239002c679cc9345
Kiel Stirling <kielstirling@gmail.com>
2017-09-16 18:14:54 +1000

    Adding first concept

--- lib/coding_task_BE_api.pm
+++ lib/coding_task_BE_api.pm
@@ -1,17 +1,23 @@
 package coding_task_BE_api;
 use Dancer2;
 
+use WordFinder;
+my $wordfinder = new WordFinder;
+
 our $VERSION = '0.1';
 
 set serializer => 'JSON';
 
 get '/wordfinder/:input' => sub {
 
-	my $input_chars = params->{ 'input' };
+	my @words = ();
+	my $input = params->{ 'input' };
 
-	my @words = qw(one two);
+	$wordfinder->alpha_chars($input);
 
-	return {@words};
+	@words = $wordfinder->build_words;
+
+	return \@words;
 };
 
 true;
