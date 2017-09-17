2b5c1433cad11b1d7a50f7e4665b368f8e8a4216
Kiel Stirling <kielstirling@gmail.com>
2017-09-17 14:46:22 +1000

    Playing with docker, removing created module -- debuging

--- lib/coding_task_BE_api.pm
+++ lib/coding_task_BE_api.pm
@@ -1,8 +1,8 @@
 package coding_task_BE_api;
 use Dancer2;
 
-use WordFinder;
-my $wordfinder = new WordFinder;
+#use WordFinder;
+#my $wordfinder = new WordFinder;
 
 our $VERSION = '0.1';
 
@@ -13,9 +13,9 @@ get '/wordfinder/:input' => sub {
 	my @words = ();
 	my $input = params->{ 'input' };
 
-	$wordfinder->alpha_chars($input);
+	#$wordfinder->alpha_chars($input);
 
-	@words = $wordfinder->build_words;
+	#@words = $wordfinder->build_words;
 
 	return \@words;
 };
@@ -25,9 +25,9 @@ get '/wordfinder2/:input' => sub {
 	my @words = ();
 	my $input = params->{ 'input' };
 
-	$wordfinder->alpha_chars($input);
+	#$wordfinder->alpha_chars($input);
 
-	@words = $wordfinder->build_words2;
+	#@words = $wordfinder->build_words2;
 
 	return \@words;
 };
